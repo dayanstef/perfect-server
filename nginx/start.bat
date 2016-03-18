99abce85ca9b1a83369905220f6d23ad04d8a762
Dejan Stefanoski <Dejan.Stefanoski@seavus.com>
2016-03-18 02:33:17 +0100

    improved start.bat

--- nginx/start.bat
+++ nginx/start.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-start /MIN cmd /k Call c:/perfect-server/nginx/mysql/bin/mysqld
-start /MIN cmd /k Call c:/perfect-server/nginx/php/php-cgi -b 127.0.0.1:9000
-start /MIN cmd /k Call c:/perfect-server/nginx/nginx
+start "" c:/perfect-server/nginx/mysql/bin/mysqld
+start /MIN "" c:/perfect-server/nginx/php/php-cgi -b 127.0.0.1:9000
+start "" c:/perfect-server/nginx/nginx
